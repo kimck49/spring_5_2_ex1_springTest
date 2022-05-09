51839fa29cf67b73c1c63cd19a4ba0c83e7626f2
kimck49 <kimck49@naver.com>
2022-05-09 15:43:53 +0900

    DI 인터페이스를 통한 부품화

---
+++ src/main/java/com/gyojincompany/spring_5_2/Pencil.java
@@ -0,0 +1,7 @@
+package com.gyojincompany.spring_5_2;
+
+public interface Pencil {
+	
+	public void use();//추상메서드 선언
+	
+}
---
+++ src/main/java/com/gyojincompany/spring_5_2/Pencil6BwithEraser.java
@@ -0,0 +1,11 @@
+package com.gyojincompany.spring_5_2;
+
+public class Pencil6BwithEraser implements Pencil {
+
+	@Override
+	public void use() {
+		// TODO Auto-generated method stub
+		System.out.println("지우개가 달린 6B 연필 사용!");
+	}
+
+}
